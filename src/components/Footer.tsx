f217a5483939f1d229948b0098713fdc6c16481b
peiyi-c <pennieyoyoyo@gmail.com>
2024-03-21 21:11:20 +0100

    create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 w-full border border-solid border-cyan-500 p-10">
+      Footer
+    </footer>
+  );
+};
+
+export default Footer;
