de7d21fc9fa0b467bf04a770da0c929086b03bcf
peiyi-c <pennieyoyoyo@gmail.com>
2024-05-16 15:20:45 +0200

    setup cookie

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react("**/react/*"), tailwind()],
+  integrations: [
+    react("**/react/*"),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
